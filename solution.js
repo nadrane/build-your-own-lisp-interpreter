281260e99223cc2df13ee46120a22dff2440296d
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-13 15:39:54 -0600

    feat(parser): support symbols

--- solution.js
+++ solution.js
@@ -8,25 +8,35 @@ function tokenize(str) {
 
 const apply = (fn, ...args) => args.reduce((accum, next) => fn(accum, next))
 
-const env = {
+const globalEnv = {
   "+": (...args) => apply((a,b) => a + b, ...args),
   "-": (...args) => apply((a,b) => a - b, ...args),
   "*": (...args) => apply((a,b) => a * b, ...args),
   "/": (...args) => apply((a,b) => a / b, ...args)
 }
 
-function evaluate(program) {
+const keywords = ['define', 'if']
+
+function evaluate(program, env={}) {
   const ast = parse(program)
-  return evaluateExpression(ast)
+  env = Object.assign(env, globalEnv)
+  // Evaluate every expression and return the value of the final one
+  return ast.map((expression) => evaluateExpression(expression, env))[ast.length - 1]
 }
 
-function evaluateExpression(expression) {
-  if (Array.isArray(expression)) {
-    const operator = expression.shift()
-    const operands = expression.map(evaluateExpression)
-    return env[operator](...operands)
-  } else {
+function evaluateExpression(expression, env) {
+  if (expression[0] == 'define') {
+    const [_, symbol, subExpression] = expression
+    env[symbol.value] = evaluateExpression(subExpression, env)
+  } else if (typeof expression === 'number') {
     return expression
+  } else if (expression instanceof Symbol) {
+    return env[expression.value] instanceof Symbol ? parseExpression(env[expression.value]) : env[expression.value]
+  }
+  else {
+    const [symbol, ...subExpresions] = expression
+    const operands = subExpresions.map(subExpression => evaluateExpression(subExpression, env))
+    return env[symbol.value](...operands)
   }
 }
 
@@ -36,14 +46,24 @@ function parse(str) {
 }
 
 function makeAst(tokens) {
+  const expressions = []
+  while(tokens[0] === '(') {
+    expressions.push(parseExpression(tokens))
+  }
+  return expressions
+}
+
+function parseExpression(tokens) {
   const ast = [];
   let token = tokens.shift();
   if (token === "(") {
     while (tokens[0] !== ")") {
-      ast.push(makeAst(tokens));
+      ast.push(parseExpression(tokens));
     }
     tokens.shift(); // Discard trailing )
     return ast
+  } else if (keywords.includes(token)) {
+    return token
   } else {
     return atom(token);
   }
@@ -53,16 +73,23 @@ function atom(token) {
   if (!isNaN(parseInt(token))) {
     return parseInt(token)
   } else {
-    return symbol(token)
+    return new Symbol(token)
   }
 }
 
-function symbol(token) {
-  return token
+// Note that we are shadowing the builtin symbol
+function Symbol(value) {
+  this.value = value
+}
+
+Symbol.prototype.valueOf = function() {
+  return this.value
 }
 
 module.exports = {
   tokenize,
   parse,
-  evaluate
+  evaluate,
+  parseExpression,
+  Symbol
 };
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-const { expect } = require("chai");
 const mocha = require("mocha");
-const { tokenize, parse, evaluate } = require("./solution");
+const chai = require("chai");
+const { expect } = chai;
+chai.use(require("./looseEqual"));
+let { tokenize, parse, evaluate, parseExpression, Symbol } = require("./solution");
 
 describe("interpreter", () => {
-
   // Parsing is the process of taking a program and transforming it into a usable data structure
   describe("parsing", () => {
     /* The first step to parsing is tokenization. At the end of the day, your program is just a text string. We want to identify each significant component in that text string and extract them into an array. */
@@ -18,45 +19,151 @@ describe("interpreter", () => {
       // note: do not assume tokens will always be 1 character in length! We will add more and longer tokens later
       it("should return an array whose elements are each a significant characters", () => {
         expect(tokenize("(* 5 5)")).to.deep.equal(["(", "*", "5", "5", ")"]);
-        expect(tokenize("(* 2 (+ 8 1 3))")).to.deep.equal(["(","*","2","(","+","8","1","3",")",")"]);
-        expect(tokenize("(/ (- 10 5) (* 2 (+ 8 1 )))")).to.deep.equal(["(","/","(","-","10","5",")","(","*","2","(","+","8","1",")",")",")"]);
+        expect(tokenize("(* 2 (+ 8 1 3))")).to.deep.equal(["(", "*", "2", "(", "+", "8", "1", "3", ")", ")"]);
+        expect(tokenize("(/ (- 10 5) (* 2 (+ 8 1 )))")).to.deep.equal([
+          "(",
+          "/",
+          "(",
+          "-",
+          "10",
+          "5",
+          ")",
+          "(",
+          "*",
+          "2",
+          "(",
+          "+",
+          "8",
+          "1",
+          ")",
+          ")",
+          ")"
+        ]);
       });
       it("appropriately tokenizes tokens longer than length 1", () => {
         expect(tokenize("(* -5 -5)")).to.deep.equal(["(", "*", "-5", "-5", ")"]);
-      })
+      });
     });
 
     /* The second step to parsing is taking the output of tokenize and transforming it into a data structure that is easier to work with, namely an a tree. This tree is referred to as an Abstract Syntax Tree, or AST for short. */
-    describe('parse', () => {
+    describe("parsing", () => {
+      describe("parseExpression", () => {
+        // I'm attempting to guide your implementation here.
+        // Our parse function later will take care of the tokenization
+        let originalParseExpression = parseExpression;
 
-      // Your parse implementation should be recursive. This should be your base case
-      it('parses an atomic unit', () => {
-        expect(parse("5")).to.equal(5)
-        expect(parse("-10")).to.equal(-10)
-        expect(parse("+")).to.equal('+')
-        expect(parse("-")).to.equal('-')
-      })
+        beforeEach(() => {
+          parseExpression = expression => originalParseExpression(tokenize(expression));
+        });
+        afterEach(() => {
+          parseExpression = originalParseExpression;
+        });
 
-      it('parses a single, unnested expression', () => {
-        expect(parse("(+ 3 5)")).to.deep.equal(['+', 3, 5]);
-      })
-      it('transforms a single, unnested expressions with a variable number of operands', () => {
-        expect(parse("(+ 3 5 2 8)")).to.deep.equal(['+', 3, 5, 2, 8]);
-        expect(parse("(- 2 5 0 2 4 2)")).to.deep.equal(['-', 2, 5, 0, 2, 4, 2]);
-      })
-      it('transforms nested expressions', () => {
-        expect(parse("(- 3 (+ 3 5))")).to.deep.equal(['-', 3, ['+', 3, 5]]);
-        expect(parse("(- 3 5 3(+ 8 0))")).to.deep.equal(['-', 3, 5, 3, ['+', 8, 0]]);
-      })
-    })
-  })
+        describe("atomic units", () => {
+          it("parses numbers", () => {
+            expect(parseExpression("5")).to.be.a("number");
+            expect(parseExpression("-10")).to.be.a("number");
 
-  describe('calculator', () => {
+            expect(parseExpression("5")).to.equal(5);
+            expect(parseExpression("-10")).to.equal(-10);
+          });
+
+          it("parses symbols", () => {
+            // Note that we are shadowing the builtin symbol with the require above
+            // You will need to represent symbols as a special class
+
+            expect(parseExpression("+")).to.be.an.instanceof(Symbol);
+            expect(parseExpression("-")).to.be.an.instanceof(Symbol);
+
+            // The loose modifer will use a '==' comparison, so you will need to define the valueOf https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/valueOf
+            // method on your Symbols so that they evaluate to the symbols they represent
+            expect(parseExpression("+")).to.loose.equal("+");
+            expect(parseExpression("-")).to.loose.equal("-");
+          });
+        });
+        describe("larger expressions", () => {
+          // Your parseExpression implementation should be recursive. This should be your base case
+          it("parses a single, unnested expression", () => {
+            expect(parseExpression("(+ 3 5)")).to.loose.equal(["+", 3, 5]);
+          });
+          it("parses a single, unnested expressions with a variable number of operands", () => {
+            expect(parseExpression("(+ 3 5 2 8)")).to.loose.equal(["+", 3, 5, 2, 8]);
+            expect(parseExpression("(- 2 5 0 2 4 2)")).to.loose.equal(["-", 2, 5, 0, 2, 4, 2]);
+          });
+          it("parses nested expressions", () => {
+            expect(parseExpression("(- 3 (+ 3 5))")).to.loose.equal(["-", 3, ["+", 3, 5]]);
+            expect(parseExpression("(- 3 5 3(+ 8 0))")).to.loose.equal(["-", 3, 5, 3, ["+", 8, 0]]);
+          });
+        });
+      });
+
+      // Your parse function should actually perform the tokenization, along with
+      // allowing sequential, unnested expressions.
+      it("parses sequential unnested expressions", () => {
+        expect(parse("(- 3 6) (+ 2 3)")).to.loose.equal([["-", 3, 6], ["+", 2, 3]]);
+        expect(parse("(- 1 0) (/ 8 0)")).to.loose.equal([["-", 1, 0], ["/", 8, 0]]);
+      });
+    });
+  });
+
+  describe("calculator", () => {
     it("Supports basic mathmatical operations", () => {
-      expect(evaluate("(+ 3 4)")).to.equal(7)
-    })
+      expect(evaluate("(+ 3 4)")).to.equal(7);
+    });
     it("Supports mathmatical operations with nesting", () => {
-      expect(evaluate("(+ 3 4 (+ 3 2))")).to.equal(12)
-    })
-  })
+      expect(evaluate("(+ 3 4 (+ 3 2))")).to.equal(12);
+    });
+    it("If multiple sequential expressions are defined, the value of the last is returned", () => {
+      expect(evaluate("(+ 3 4)(+ 3 2)")).to.equal(5);
+    });
+  });
+
+  /* Variables are set in lisp using the `define` keyword.
+     For example,the following expression sets x equal to 10:
+
+     (define x 10)
+
+     We are not required to make the x equal to a constant. In fact,
+     in fact, we can make it equal to any expression as well:
+
+     (define x (+ 3 2))
+  */
+  describe("define", () => {
+    /* Any define expression will not have a meaningful return value. Instead, it modifies the global environment
+       We're going to have evaluate take in an environment, and if it sees the define keyword, it will modify that environment.
+
+    */
+
+    it("can define a variable to equal a constant", () => {
+      const env = {};
+      evaluate("(define x 10)", env);
+      expect(env).to.have.property("x", 10);
+    });
+
+    it("can define multiple unnested expressions", () => {
+      const env = {};
+      expect(evaluate("(define x 2)(define y 5)", env));
+      expect(env).to.have.property("x", 2);
+      expect(env).to.have.property("y", 5);
+    });
+
+    it("symbol definitions can be used later in the program", () => {
+      const env = {};
+      expect(evaluate("(define x 2)(+ x 3)", env)).to.equal(5)
+      expect(env).to.have.property("x", 2);
+    });
+
+    it("symbol definitions will be recursively followed", () => {
+      let env = {};
+      expect(evaluate("(define x 2)(define y x)(+ y 5)", env)).to.equal(7);
+      expect(env).to.have.property("x", 2);
+      expect(env).to.have.property("y", 2);
+
+      env = {};
+      expect(evaluate("(define x 4)(define y x)(define z y)(+ z 5)", env)).to.equal(9);
+      expect(env).to.have.property("x", 4);
+      expect(env).to.have.property("y", 4);
+      expect(env).to.have.property("z", 4);
+    });
+  });
 });
